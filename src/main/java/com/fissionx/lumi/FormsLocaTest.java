9f05f1712cb5d02ad75b364512bbae2f16527e9a
fissionx-ai <support@fissionx.ai>
2025-02-24 22:44:09 +0530

    lumi project init

---
+++ src/main/java/com/fissionx/lumi/FormsLocaTest.java
@@ -0,0 +1,18 @@
+package com.fissionx.lumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+
+public class FormsLocaTest {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FormsLocaTest.class, args);
+
+	}
+
+}
